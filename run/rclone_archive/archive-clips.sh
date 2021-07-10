f7eb2b191947f21cf49e3c47b236ecdcb7e55f2e
Andy Poirier <andpoi@gmail.com>
2021-07-10 14:14:56 -0500

    Update archive-clips.sh

--- run/rclone_archive/archive-clips.sh
+++ run/rclone_archive/archive-clips.sh
@@ -1,4 +1,26 @@
 #!/bin/bash -eu
+#!/bin/bash -eu
+
+function connectionmonitor {
+  while true
+  do
+    for _ in {1..5}
+    do
+      if timeout 6 /root/bin/archive-is-reachable.sh "$ARCHIVE_SERVER"
+      then
+        # sleep and then continue outer loop
+        sleep 5
+        continue 2
+      fi
+    done
+    log "connection dead, killing archive-clips"
+    killall -9 rclone || true
+    kill -9 "$1" || true
+    return
+  done
+}
+
+connectionmonitor $$ &
 
 flags=("-L" "--transfers=1")
 if [[ -v RCLONE_FLAGS ]]
